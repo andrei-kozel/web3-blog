37b22f0d67c02ff38e4e5a64c601ae29b7b763c2
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-04-21 20:25:02 +0200

    deployed to ropsten

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  export const contractAddress = "0x6FCacE10783e9768a53f29437Ce07318CeaCEF9f"
+  export const ownerAddress = "0xd74E5eCFcaEdcf9DC3DC221a5963af5EB629dCbf"
   
