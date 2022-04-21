34db8b168d5afcc8d43b16eaac80512f19af2dcb
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-04-21 20:25:14 +0200

    added ropsten config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     hardhat: {
       chianId: 1337,
+    },
+    ropsten: {
+      url: `${process.env.ROPSTEN_NETWORK_URL}`,
+      accounts: [`${process.env.PRIVATE_KEY}`],
     },
   },
 };
